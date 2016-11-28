137ba6bf4740336c21a5e80a8e16fc3f7c5463ac
Arun Kumar <arunkumar31786@gmail.com>
2016-11-28 11:33:37 +0530

    dom

---
+++ Day1/03-external.js
@@ -0,0 +1,5 @@
+			console.log("i am js");
+			var heads = document.getElementsByTagName('h1');
+			var secondHead = heads[1]
+			console.log(secondHead)
+			secondHead.innerHTML = "Hello JavaScript";
