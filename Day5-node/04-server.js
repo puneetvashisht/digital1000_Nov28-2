5d10ac9ccfb2c8a00f921c6d0213dbef61de09ab
Arun Kumar <arunkumar31786@gmail.com>
2016-12-02 12:49:11 +0530

    static dir

--- Day5-node/04-server.js
+++ Day5-node/04-server.js
@@ -4,22 +4,24 @@ var server = express();
 var port = 7777;
 var host = "localhost";
 
+server.use(express.static(__dirname+"/WebContent"));
+
 server.get('/hello/:name', function(req, res){
 	console.log(req.params.name)
 	console.log("req is coming with hello url and get method");
 //	res.send("<h1>Welcome "+req.params.name+"</h1>")
 	res.send({success: true, message: req.params.name})
-})
+});
 
 server.post('/hello', function(req, res){
 	console.log("req is coming with hello url and post method");
 	res.send({success: false})
-})
+});
 
 server.put('/hello', function(req, res){
 	console.log("req is coming with hello url and put method");
 	res.send({success: false})
-})
+});
 
 server.listen(port, host, function(err){
 	if(err){
